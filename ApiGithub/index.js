381014f3088a8558c08a5f8af2ad206e72351692
Michael SN <michael.dev.nascimento@gmail.com>
2022-10-19 11:00:06 -0300

    usando axios no backend

--- ApiGithub/index.js
+++ ApiGithub/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
-app.listen("3000");
+app.listen("3000", () => console.log("Running ..."));
 
 // middleware
 app.use(express.json());
+
+app.route("/").get((req, res) => {
+  axios
+    .get("https://api.github.com/users/michael-sn")
+    .then((result) =>
+      res.send(
+        `<img style="width: 150px; height: 150px" src="${result.data.avatar_url}"/>`
+      )
+    )
+    .catch((error) => console.log(error));
+});
